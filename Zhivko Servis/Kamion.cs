2abb0ae763c70dfe14a95ff3e98a5a37e87ee8f6
zikica97 <zivko_kolev97@hotmail.com>
2019-08-10 13:43:18 +0200

    Add project files.

---
+++ Zhivko Servis/Kamion.cs
@@ -0,0 +1,52 @@
+﻿namespace Zhivko_Servis
+{
+    partial class Program
+    {
+        class Kamion
+        {
+            private string TipNaGorivo { get; set; }
+            private int KapacitetNaRezervoar { get; set; }
+            private int PotrosuvackaNa100 { get; set; }
+            private int Cena { get; set; }
+            private string Boja { get; set; }
+            private int GodinaNaProizvodstvo { get; set; }
+            private string Problem { get; set; }
+
+
+            Kamion()
+            {
+            }
+
+            Kamion(string TipNaGorivo, int KapacitetNaRezervoar,
+                int PotrosuvackaNa100, int Cena, string Boja,
+                int GodinaNaProizvodstvo, string Problem)
+
+            {
+                string tipnagorivo = TipNaGorivo;
+                int kapacitetNaRezervoar = KapacitetNaRezervoar;
+                int potrosuvackaNa100 = PotrosuvackaNa100;
+                int cena = Cena;
+                string boja = Boja;
+                int godinaNaProizvodstvo = GodinaNaProizvodstvo;
+                string problem = Problem;
+
+
+            }
+
+
+            public float MaksimalenDomet(int kapacitetNaRezervoar, int potrosuvackaNa100)
+            {
+                float maksimalendomet = (kapacitetNaRezervoar / potrosuvackaNa100);
+                return maksimalendomet;
+            }
+
+            public int MomentalnaVrednost(int cena, int godinaNaProizvodstvo, int potrosuvacka)
+            {
+                int momentalnaVrednost = cena - (2 * (cena - (godinaNaProizvodstvo / 100) * potrosuvacka));
+                return momentalnaVrednost;
+            }
+
+
+        }
+    }
+}
---
+++ Zhivko Servis/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhivko_Servis
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Dobredojdovte vo servisot na Zhivko ! Posluzete se so naseto meni za poveke informacii !! ");
+            //int brojac = 1;
+
+            //while (brojac == 1)
+            //{
+            //    Console.WriteLine("1) Vnes na vozilo");
+            //    Console.WriteLine("2) Pregled na veke vneseno vozilo");
+            //    Console.WriteLine("3) Izlez");
+
+
+            //    string priem = Console.ReadLine();
+            //    int priemKonv = 0;
+
+
+
+            //    KonvertNaBroj(priem, priemKonv);
+            //    if (KonvertNaBroj(priem, priemKonv) == 1)
+            //    {
+
+            //        Console.Clear();
+            //        Console.WriteLine("Tip na Vozilo ");
+            //        Console.WriteLine("1) Kopneno");
+            //        Console.WriteLine("2) Vodeno");
+            //        Console.WriteLine("3) Vozdusno");
+
+
+            //        string tipNaVozilo = Console.ReadLine();
+            //        if(KonvertNaBroj(tipNaVozilo, priemKonv) == 1)
+            //        {
+            //            Console.Clear();
+            //            Console.WriteLine("Kakvo e Kopnenoto Vozilo? ");
+            //            Console.WriteLine("1) Patnicko");
+            //            Console.WriteLine("2) Tovarno");
+
+            //            string kopnenoVozilo = Console.ReadLine();
+
+
+            //            if (KonvertNaBroj(kopnenoVozilo, priemKonv) == 1)
+            //            {
+
+            //                Console.Clear();
+            //                Console.WriteLine("Kakvo e Kopnenoto Patnicko Vozilo? ");
+            //                Console.WriteLine("1) Avtomobil Obicen");
+            //                Console.WriteLine("2  Avtomobil Sportski");
+            //                Console.WriteLine("")
+
+
+            //            }
+
+
+        }
+        int KonvertNaBroj(string priem1, int priemKonv1)
+        {
+
+            int.TryParse(priem1, out priemKonv1);
+            return priemKonv1;
+
+        }
+    }
+    class KopnenoVozilo
+    {
+        public string Patnicko { get; set; }
+        public string Tovarno { get; set; }
+
+
+
+    }
+}
